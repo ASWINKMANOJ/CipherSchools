649c7861e9b37e04e34c62a8c6b902854d6f5453
ASWIN K MANOJ <aswinkmanoj101@gmail.com>
2023-10-04 00:26:01 +0530

    Learned about variable, constants and data types in c++. eg: int,float,bool,string,char

---
+++ Week-1-DSA-CipherSchools/Lecture-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    cout << a << endl;
+    float b = 1.0003;
+    cout << b << endl;
+    string c = "Hello World!";
+    cout << c << endl;
+    char d = 'h';
+    cout << d << endl;
+    bool e = true;
+    cout << boolalpha << e << endl;
+
+    return 0;
+}
