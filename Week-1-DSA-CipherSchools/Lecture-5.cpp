c546bdf1dee8bba302771b3cf81d48cb92eb1b21
ASWIN K MANOJ <aswinkmanoj101@gmail.com>
2023-10-06 22:45:42 +0530

    Learned about arrays

--- Week-1-DSA-CipherSchools/Lecture-5.cpp
+++ Week-1-DSA-CipherSchools/Lecture-5.cpp
@@ -3,20 +3,14 @@ using namespace std;
 
 int main()
 {
-    int arr[5];
     int a, b, c, d, e;
 
     cout << "Enter Five numbers: " << endl;
 
     cin >> a >> b >> c >> d >> e;
-    arr[0] = a;
-    arr[1] = b;
-    arr[2] = c;
-    arr[3] = d;
-    arr[4] = e;
 
     cout << "The numbers in reverse in order : " << endl;
-    cout << arr[4] << " " << arr[3] << " " << arr[2] << " " << arr[1] << " " << arr[0] << endl;
+    cout << e << " " << d << " " << c << " " << b << " " << a << endl;
 
     return 0;
 }
